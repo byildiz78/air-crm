e8e8e30af4a829a3059039d7ead22b3f077f8c7c
unknown <byildiz33@gmail.com>
2025-08-06 17:56:38 +0300

    ds

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -59,6 +59,7 @@ export default function LoginPage() {
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                autoComplete="email"
                 required
                 disabled={isLoading}
               />
@@ -71,6 +72,7 @@ export default function LoginPage() {
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
                 required
                 disabled={isLoading}
               />
